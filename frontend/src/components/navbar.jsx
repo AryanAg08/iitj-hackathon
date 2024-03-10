79c7b076b2e2ab90a058f1f66b101dc9f2f9742e
Prakriti Yadav <prakritiyadav2004@gmail.com>
2024-03-10 17:45:54 +0530

    Navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
 
-export class navbar extends Component {
-  static propTypes = {}
 
-  render() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AiOutlineHome, AiOutlineInfoCircle, AiOutlineLogin, AiOutlineUser, AiOutlineUserAdd, AiOutlineUserSwitch, AiOutlineMail } from 'react-icons/ai';
+
+
+const Navbar = () => {
     return (
-      <div>navbar</div>
-    )
-  }
-}
+    
+        
+      <nav className="flex flex-row items-center justify-between bg-slate-900 text-white py-4 px-6">
+        
+        <div className="hidden lg:flex md:flex lg:flex-1 items-center justify-end font-normal space-x-6">
+          <Link to="Home" spy={true} smooth={true} className="hover:bg-slate-800 hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineHome size={20} /> Home
+          </Link>
+          <Link to="About" spy={true} smooth={true} className="hover:bg-slate-800 hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineInfoCircle size={20}/> About
+          </Link>
+          <Link to="Contact"spy={true} smooth={true}  className="hover:bg-slate-800 hover:bg-slate-800  hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineMail size={20} /> Contact
+          </Link>
 
-export default navbar
+          <Link to="SignUp" spy={true} smooth={true} className="hover:bg-teal-800 hover:text-white-500 hover:rounded  transition py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineUserAdd size={20}/> <b>SignUp</b>
+          </Link>
+
+          <Link to="LogIn" spy={true} smooth={true} className="hover:bg-teal-800  hover:text-white-500 hover:rounded transition py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineLogin size={20}/><b> LogIn</b>
+          </Link>
+        </div>
+        <div className="lg:hidden block bg-slate-900 text-white py-4 px-6 ">
+          <ul className="flex text-center text-xl space-x-6">
+           
+          </ul>
+        </div>
+      </nav>
+      
+    );
+  };
+  
+  export default Navbar;
